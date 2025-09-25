ba40a4a2aea084dd04b03e98d4e5d23b7ef1b058
md7147 <md7147@g.rit.edu>
2025-09-25 12:14:22 +0400

    Largenumber

---
+++ largenumber.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the first number="))
+b = int(input("Enter the second number="))
+if a > b:
+    print("a is greater")
+elif a < b:
+    print("b is greater")
+else:
+    print("a and b are equal")
+        
